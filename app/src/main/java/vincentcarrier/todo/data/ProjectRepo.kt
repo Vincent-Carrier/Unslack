4f87cffa671d41bd803bb82d3a8be2965100ed6f
Vincent <vincentcarrier15@gmail.com>
2017-10-30 23:56:18 -0400

    Add Dao interface

--- app/src/main/java/vincentcarrier/todo/data/ProjectRepo.kt
+++ app/src/main/java/vincentcarrier/todo/data/ProjectRepo.kt
@@ -1,28 +1,28 @@
 package vincentcarrier.todo.data
 
 import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
-import io.objectbox.Box
-import io.objectbox.rx.RxQuery
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import vincentcarrier.todo.App
+import vincentcarrier.todo.data.local.CommandDao
+import vincentcarrier.todo.data.local.ProjectDao
+import vincentcarrier.todo.data.local.TaskDao
 import vincentcarrier.todo.data.remote.TodoistService
-import vincentcarrier.todo.models.Command
 import vincentcarrier.todo.models.Project
 import vincentcarrier.todo.models.Task
 
 
 class ProjectRepo(private val service: TodoistService = TodoistService()) {
 
-  private val projectBox: Box<Project> = App.boxStore.boxFor(Project::class.java)
-  private val taskBox: Box<Task> = App.boxStore.boxFor(Task::class.java)
-  private val commandBox = App.boxStore.boxFor(Command::class.java)
+  private val projectDao = ProjectDao()
+  private val taskDao = TaskDao()
+  private val commandDao = CommandDao()
 
   fun whenProjectsLoaded(): Observable<List<Project>> =
       ReactiveNetwork.observeNetworkConnectivity(App.instance)
       .observeOn(Schedulers.io())
       .flatMap { connectivity ->
-        if (connectivity.isAvailable) loadFromNetwork() else loadFromDisk()
+        if (connectivity.isAvailable) loadFromNetwork() else projectDao.whenLoaded()
       }
 
   private fun loadFromNetwork() =
@@ -31,14 +31,10 @@ class ProjectRepo(private val service: TodoistService = TodoistService()) {
         response.projects.map { Project(it) } to response.items.map { Task(it) }
       }
       .doOnNext { response ->
-        projectBox.put(response.first)
-        taskBox.put(response.second)
+        projectDao.put(response.first)
+        taskDao.put(response.second)
       }
       .map { response ->
         response.first
       }
-
-  private fun loadFromDisk() =
-      RxQuery.observable(projectBox.query().build())
-      .subscribeOn(Schedulers.io())
 }
--- app/src/main/java/vincentcarrier/todo/data/TaskRepo.kt
+++ app/src/main/java/vincentcarrier/todo/data/TaskRepo.kt
@@ -1,19 +1,19 @@
 package vincentcarrier.todo.data
 
 import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
-import io.objectbox.rx.RxQuery
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import vincentcarrier.todo.App
+import vincentcarrier.todo.data.local.CommandDao
+import vincentcarrier.todo.data.local.TaskDao
 import vincentcarrier.todo.data.remote.TodoistService
-import vincentcarrier.todo.models.Command
 import vincentcarrier.todo.models.Task
 import vincentcarrier.todo.models.Task_
 
 class TaskRepo(private val projectId: Long, private val service: TodoistService = TodoistService()) {
 
-  private val taskBox = App.boxStore.boxFor(Task::class.java)
-  private val commandBox = App.boxStore.boxFor(Command::class.java)
+  private val taskDao = TaskDao(projectId)
+  private val commandDao = CommandDao()
 
   fun whenTasksLoaded(): Observable<List<Task>> =
       ReactiveNetwork.observeNetworkConnectivity(App.instance)
@@ -23,9 +23,7 @@ class TaskRepo(private val projectId: Long, private val service: TodoistService 
         else loadFromDisk()
       }
 
-  private fun loadFromDisk() =
-      RxQuery.observable(taskBox.query().equal(Task_.projectId, projectId).build())
-      .subscribeOn(Schedulers.io())
+  private fun loadFromDisk() = taskDao.whenLoaded(taskDao.box.query().equal(Task_.projectId, projectId))
 
   private fun loadFromNetwork() =
       service.whenTasksLoaded()
@@ -36,10 +34,13 @@ class TaskRepo(private val projectId: Long, private val service: TodoistService 
       }
 
   fun put(task: Task) {
-//    commandBox.put(Command(Commands.ITEM_ADD, task))
-    taskBox.put(task.apply { project.targetId = projectId })
+//    commandDao.put(Command(Commands.ITEM_ADD, task))
+    taskDao.put(task.apply { project.targetId = projectId })
   }
 
-  fun remove(task: Task) = taskBox.remove(task)
+  fun remove(task: Task) {
+//    commandDao.put(Command(Commands.ITEM_REMOVE, task))
+    taskDao.remove(task)
+  }
 }
 
