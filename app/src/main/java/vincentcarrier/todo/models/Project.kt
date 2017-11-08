2f38bf8f22f527e169aba809ff81f3a70c0e1307
Vincent <vincentcarrier15@gmail.com>
2017-11-08 13:35:34 -0500

    Switch back to separate JSON models for database entities

--- app/src/main/java/vincentcarrier/todo/models/Project.kt
+++ app/src/main/java/vincentcarrier/todo/models/Project.kt
@@ -1,6 +1,5 @@
 package vincentcarrier.todo.models
 
-import com.squareup.moshi.Json
 import io.objectbox.annotation.Backlink
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
@@ -8,11 +7,10 @@ import io.objectbox.relation.ToMany
 
 @Entity
 class Project {
+
   @Id(assignable = true)
-  @Json(name = "id")
   var id: Long = 0
 
-  @Json(name = "name")
   lateinit var name: String
 
   @Backlink lateinit var tasks: ToMany<Task>
@@ -21,14 +19,18 @@ class Project {
   constructor() {}
 
   // For user
-  constructor(name: String, tasks: ToMany<Task>) {
+  constructor(name: String) {
     this.name = name
-    this.tasks = tasks
   }
 
   // For Moshi
-  constructor(id: Long, name: String) {
-    this.id = id
-    this.name = name
+  internal constructor(json: ProjectJson) {
+    this.id = json.id
+    this.name = json.name
   }
 }
+
+internal data class ProjectJson(
+    val id: Long,
+    val name: String
+)
--- app/src/main/java/vincentcarrier/todo/models/Task.kt
+++ app/src/main/java/vincentcarrier/todo/models/Task.kt
@@ -1,28 +1,21 @@
 package vincentcarrier.todo.models
 
-import com.squareup.moshi.Json
 import io.objectbox.annotation.Convert
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
-import io.objectbox.annotation.Transient
 import io.objectbox.relation.ToOne
 import org.joda.time.LocalDateTime
 
 @Entity
 class Task {
   @Id(assignable = true)
-  @Json(name = "id")
   var id: Long = 0
 
-  @Json(name = "content")
   lateinit var name: String
 
-  @Json(name = "date_added")
   @Convert(converter = LocalDateTimeConverter::class, dbType = Long::class)
   lateinit var dateAdded: LocalDateTime
 
-  @Transient
-  @Json(name = "project_id")
   private var projectId: Long = 0
 
   lateinit var project: ToOne<Project>
@@ -38,11 +31,18 @@ class Task {
   }
 
   // For Moshi
-  constructor(id: Long, name: String, dateAdded: LocalDateTime, projectId: Long) {
-    this.id = id
-    this.name = name
-    this.dateAdded = dateAdded
+  internal constructor(json: TaskJson) {
+    this.id = json.id
+    this.name = json.content
+    this.dateAdded = json.date_added
     this.projectId = projectId
     this.project.targetId = projectId
   }
 }
+
+internal data class TaskJson(
+    val id: Long,
+    val content: String,
+    val date_added: String,
+    val project_id: Long
+)
